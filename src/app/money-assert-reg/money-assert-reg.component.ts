f06a7b4241896cced21c3a087266fc944a2083df
cocohuQQ <956140151@qq.com>
2020-05-02 15:46:48 +0800

    资产登记

--- src/app/money-assert-reg/money-assert-reg.component.ts
+++ src/app/money-assert-reg/money-assert-reg.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-money-assert-reg',
@@ -20,7 +22,8 @@ export class MoneyAssertRegComponent implements OnInit {
 
   constructor(
     private httpClient: HttpClient,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+    private message: NzMessageService) {
       this.queryForm();
   }
 
@@ -53,6 +56,49 @@ export class MoneyAssertRegComponent implements OnInit {
          this.validateForms.push(this.formBuilder.group(fromBuilderJson));
         }
       });
+  }
+
+  save(): void {
+    const data  = [];
+    this.validateForms.forEach((form, index) => {
+      // tslint:disable-next-line:forin
+      for  (const key in form.value) {
+        data.push({
+          peopleCode : this.tabsValue[index],
+          assetsTypeCode: key,
+          value: form.value[key],
+          date : this. getyyyyMMdd()
+        });
+      }
+    });
+    this.httpClient.post(environment.TreasureBaseUrl + 'assetReg/regAssetForAllPeople',
+    data, {}).subscribe(
+      response => {
+        this.message.success('保存成功');
+      }
+    );
+
+  }
+
+  getyyyyMMdd() {
+    const d = new Date();
+    let curr_date = d.getDate() + '';
+    let curr_month = (d.getMonth() + 1) + '';
+    const curr_year = d.getFullYear();
+    curr_month = String(curr_month).length < 2 ? ("0" + curr_month): curr_month;
+    curr_date = String(curr_date).length < 2 ? ("0" + curr_date): curr_date;
+    const yyyyMMdd = curr_year + "" + curr_month +""+ curr_date;
+    return yyyyMMdd;
+}
+
+  isValidForms(): boolean {
+    let valide = true;
+    this.validateForms.forEach((form) => {
+      if ( !form.invalid ) {
+        valide =  form.invalid;
+      }
+    });
+    return valide;
   }
 }
 export interface AssetItem {
