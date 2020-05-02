a2cb6a4bf72a91e78d17f656b3311723bcba0d4b
cocohuQQ <956140151@qq.com>
2020-05-02 14:01:00 +0800

    资产登记

--- src/app/money-assert-reg/money-assert-reg.component.ts
+++ src/app/money-assert-reg/money-assert-reg.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-money-assert-reg',
@@ -9,9 +10,57 @@ import { environment } from 'src/environments/environment';
 })
 export class MoneyAssertRegComponent implements OnInit {
 
-  constructor() { }
+  validateForms: FormGroup[] = [];
 
-  ngOnInit() {
+  tabsTitle: string[] = [];
+
+  tabsValue: string[] = [];
+
+  itemsComponentForAllTab: any = [];
+
+  constructor(
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder) {
+      this.queryForm();
   }
 
+  ngOnInit() {
+
+  }
+
+  queryForm(){
+    this.httpClient.post(environment.TreasureBaseUrl + 'assetReg/items', {}, {})
+    .subscribe((returnData: Map<string, any>) => {
+      if ( !returnData || returnData.size <= 0 ) {
+        return;
+      }
+      // tslint:disable-next-line:forin
+      for (const curPeople in returnData) {
+         const assetItems: Array<AssetItem> = returnData[curPeople].assets;
+         const fromBuilderJson  = {};
+         const itemsComponentForCurTab: any = [];
+         assetItems.forEach(curAssetItem  => {
+          const item = {
+            'lableTxt': curAssetItem.assetsTypeDescrible, //应收帐款
+            'formControlName': curAssetItem.assetsTypeCod
+          }
+          itemsComponentForCurTab.push(item);
+          fromBuilderJson[curAssetItem.assetsTypeCod] =  [0, [Validators.required]]
+         });
+         this.tabsTitle.push(returnData[curPeople].peopleName);
+         this.tabsValue.push(curPeople);
+         this.itemsComponentForAllTab.push(itemsComponentForCurTab);
+         this.validateForms.push(this.formBuilder.group(fromBuilderJson));
+        }
+      });
+  }
+}
+export interface AssetItem {
+  assetsTypeCod: number;
+
+  assetsTypeDescrible: string;
+
+  peopleCode: number;
+
+  peopleName: string;
 }
